94e5ed0c66dcc1888d9dcb368e265ffcf40ff103
Luke MacLean <40969439+MacLeanLuke@users.noreply.github.com>
2021-07-12 21:53:34 -0500

    initial commit

---
+++ server.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, redirect, session
+
+
+app = Flask(__name__)
+app.secret_key = 'keep it secret, keep it safe'
+
+@app.route('/')
+def index():
+    if 'clicks' not in session:
+        session['clicks'] = 0
+    else:
+        session['clicks'] += 1
+    # session.modified = True
+    return render_template('index.html')
+
+@app.route('/add_click', methods=['POST'])
+def add_click():
+    # session['clicks'] = True
+    session['clicks'] += 1
+    session.modified = True
+    return redirect('/')
+
+@app.route('/reset_click', methods=['POST'])
+def reset_click():
+    session.clear()
+    session.modified = True
+    return redirect('/')
+
+    
+
+if __name__=="__main__":
+    app.run(debug=True)
